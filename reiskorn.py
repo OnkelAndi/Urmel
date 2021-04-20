438bfca828e66e79dc56dc477c96177172cc7282
OnkelAndi <andreas@wrenger.com>
2021-04-20 17:38:25 +0200

    Test5

--- reiskorn.py
+++ reiskorn.py
@@ -6,8 +6,8 @@ for feld in range(64):
     print(f"Feld {feld+1}. = {reiskörner:>27} Reiskörner und damit insgesamt"
           f"{summe:>28,} Reiskörner")
 
-
 gewicht = summe * 0.02 / 1000 / 1000
+print()
 print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
       f" Reiskörner {gewicht:,.0f} Tonnen")
